1b8d9aea82998f32b78e0b0f456c45029e91c12b
jbrunton <john_brunton@hotmail.co.uk>
2012-05-06 16:17:30 +0100

    added registerResource function to Environment obj ctor

--- types/environment.js
+++ types/environment.js
@@ -31,16 +31,104 @@ define([
             return _resources[type_name];
         };
         
-        this.registerSimpleType("list",
-            function(arr, itemTy) {
-                return arr;
-            },
-            function(arr, itemTy) {
-                return _.map(arr, function(x) {
-                    return itemTy.deserialize(x);
-                });
+        this.registerResource = function(params) {
+            var objCtor = params.objCtor,
+                typeName = params.typeName,
+                collectionName = params.collectionName;
+                
+            if (!objCtor) {
+                objCtor = function(data) {
+                    this.deserialize(data || {});
+                };
             }
-        );
+                
+            _resources[typeName] = objCtor;
+    
+            var httpResource = new HttpResource(collectionName);
+            
+            objCtor.prototype.type_name = typeName;
+            
+            objCtor.prototype.serialize = function() {
+                return serialize(this);
+            };
+        
+            objCtor.prototype.deserialize = function(data) {
+                deserialize(data, this);
+                return this;
+            };
+            
+            var applyExtensions = function(obj, opts) {
+                app.core.extend(obj, opts.extensions);
+                
+                if (opts.includes) {
+                    _.each(obj.includes, function(fieldOpts, fieldName) {
+                        app.core.extend(obj[fieldName], fieldOpts);
+                    });
+                }
+            };
+            
+            objCtor.prototype.load = function(id, reqOpts) {
+                var self = this;
+                
+                this.id(id);
+                
+                httpResource.doReadReq(id, function(data) {
+                    deserialize(data, self);
+                }, null, reqOpts);
+                
+                applyExtensions(self, reqOpts);
+                
+                return this;
+            };
+            
+            objCtor.prototype.save = function(success, options) {
+                var self = this;
+                
+                var data = serialize(this, options);
+                if (this.id() > 0) {
+                    httpResource.doPutReq(data, function(data) {
+                        deserialize(data, self);
+                        if (success) {
+                            success(self);
+                        }
+                    });
+                } else {
+                    httpResource.doPostReq(data, function(data) {
+                        deserialize(data, self);
+                        if (success) {
+                            success(self);
+                        }
+                    });
+                }
+                
+                return this;
+            };
+            
+            objCtor.load = function(id, success, error, reqOpts) {
+                var self = this;
+                
+                httpResource.doReadReq(id, function(data) {
+                    var obj = new objCtor();
+                    obj.deserialize(data);
+                    success(obj);
+                });
+            };
+            
+            objCtor.loadCollection = function(reqOptions, success, error) {
+                var self = this;
+                
+                httpResource.doReadCollectionReq(function(collectionData) {
+                    var collection = _.map(collectionData, function(data) {
+                        var obj = new objCtor();
+                        obj.deserialize(data);
+                        return obj;
+                    });
+                    if (success) {
+                        success(collection);
+                    }
+                }, null, reqOptions);
+            };
+        };
 
     };
     
